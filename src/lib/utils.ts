0aacaf12ea1a69f86220b51a48d99a0b22272ae1
akshayjthummar <akshaythummar34@gmail.com>
2024-05-12 23:29:37 +0530

    initial

---
+++ src/lib/utils.ts
@@ -0,0 +1,45 @@
+import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const formatPrice = (price: number) => {
+  const formatter = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+  });
+  return formatter.format(price);
+};
+
+export function contructMetaData({
+  title = "CaseCobra - custom high-quality",
+  description = "Create custom hegh-quality phone cases in seconds",
+  image = "/thubnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@akshayjthummar",
+    },
+    icons,
+  };
+}
